aa9e410575a01bc0e6d0a6467ea8660b989d5e14
madebymak <mr.jmak@gmail.com>
2016-11-09 00:47:45 +0000

    Added link on line 14

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL:req.params.id };
+  let templateVars = { shortURL:req.params.id, longURL: urlDatabase[req.params.id]}; //here
   res.render("urls_show", templateVars);
 });
 
