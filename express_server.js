857a8d49cadbb5e19d09894a8c8fb67aebcd47d7
madebymak <mr.jmak@gmail.com>
2016-11-09 00:22:34 +0000

    Added url_show.ejs w/ HTML fill

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,8 +14,13 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL:req.params.id };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase}
+  let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
