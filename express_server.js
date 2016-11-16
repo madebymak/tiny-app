e993fd1fa972479ccbe465c8de7e2be15290f6af
madebymak <mr.jmak@gmail.com>
2016-11-16 08:10:39 +0000

    Made some changes to try to troubleshoot log in feature

--- express_server.js
+++ express_server.js
@@ -8,14 +8,10 @@ const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
 
-// app.use(function (req, res, next) {
-//   let idUserCookie = req.cookies["user_id"];
-//   req.currentUser = users[idUserCookie];
-//   next();
-// })
-
 app.set("view engine", "ejs");
 
+
+///Database////////////////
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -28,37 +24,43 @@ email: "test@test.com",
 password: "test1234"}
 };
 
-app.get("/", (req, res) => {
+/////////////////////////////
 
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/", (req, res) => {
   res.redirect("/urls")
 });
 
-app.post("/urls", (req, res) => {
-  var shortURL = generateRandomString(); //generates random string
-  var longURL = req.body.longURL; //sets key-values pairs from bodyparse
-  urlDatabase[shortURL] = longURL; //sets longURL to urlDatabase[shortURL]
-  res.redirect(`/urls/${shortURL}`); //redirects
-});
-
-//login
+//login////////////////////
 app.get("/login", (req, res) => {
   //res.send("login");
-  res.render("/urls_login")
+  res.render("/login")
 });
 
-
+//made changes
 app.post("/login", (req, res) => {
-  res.cookie("id", req.body.username);
+  //if !email, return 403
+  //if password  !== saved password, return 403
+  //if email && password, set user_id cookie
+  // req.cookies.user
   res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("email");
   res.redirect("/");
 });
 ///////////////////////
 
-//user registration
+//user registration////////////
+
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
 
 app.post("/register", (req, res) => {
   let userRandomID = generateRandomString();
@@ -70,8 +72,6 @@ app.post("/register", (req, res) => {
   }
 
   for(var userId in usersObj) {
-    //console.log("Input:", req.body.email);
-    //console.log("Check:", usersObj[userId].email)
     if (usersObj[userId].email === req.body.email) {
       res.status(400).send("Email not available.");
       return;
@@ -85,16 +85,31 @@ app.post("/register", (req, res) => {
 
 })
 
-app.get("/register", (req, res) => {
-  res.render("urls_register");
+////////////////////////////////////////
+
+
+
+app.get("/urls", (req, res) => {
+//   let templateVars = { email: req.cookies["email"],
+// urls: urlDatabase};
+  // res.render("urls_index", templateVars);
+  res.render("urls_index");
+});
+
+app.post("/urls", (req, res) => {
+  let email = req.cookie.usersObj["email"];
+  var shortURL = generateRandomString(); //generates random string
+  var longURL = req.body.longURL; //sets key-values pairs from bodyparse
+  urlDatabase[shortURL] = longURL; //sets longURL to urlDatabase[shortURL]
+  res.redirect(`/urls/${shortURL}`); //redirects
 });
 
 
-////////////////////////////////////////
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"]};
-  res.render("urls_new", templateVars);
+  // let templateVars = { email: req.cookies["email"]};
+  // res.render("urls_new", templateVars);
+  res.render("urls_new");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -103,7 +118,7 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { username: req.cookies["username"], shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = { email: req.cookies["email"], shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
@@ -112,24 +127,12 @@ app.post("/urls/:id", (req, res) => {
   res.redirect('/urls');
 });
 
-////made changes
-app.get("/urls", (req, res) => {
-  let templateVars = {email: req.cookies["email"],
-urls: urlDatabase};
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+//Functions//////////////////////
 
 function generateRandomString() {
 
@@ -142,6 +145,5 @@ function generateRandomString() {
   return newURL;
 }
 
-
-
+//////////////////////////////////
 
