a46831b0e1fc377f260bc754b9c2a64b4f529508
madebymak <mr.jmak@gmail.com>
2016-11-19 17:54:41 +0000

    Added routes and functions

--- express_server_v2.js
+++ express_server_v2.js
@@ -20,6 +20,80 @@ const databaseUsers = {
 
 ////////////////
 
-app.set('view engine', "ejs");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Routes//////////
+
+app.get('/', function (req, res) {
+  res.redirect("/login")
+});
+
+app.get('/urls', function (req, res) {
+  //TODO
+});
+
+app.get('/urls/new', function (req, res) {
+  // TODO
+});
+
+app.get('/urls/:id', function (req, res) {
+  // TODO
+});
+
+app.get('/u/:id', function (req, res) {
+  //TODO
+});
+
+app.post('/urls', function (req, res) {
+  // TODO
+});
+
+app.post('/urls/:id', function (req, res) {
+  // TODO
+});
+
+app.get('/login', function (req, res) {
+  res.send("Login")
+});
+
+app.get('/register', function (req, res) {
+  // TODO
+});
+
+app.post('/register', function (req, res) {
+  // TODO
+});
+
+app.post('/login', function (req, res) {
+  //TODO
+});
+
+app.post('/logout', function (req, res) {
+  //TODO
+});
+
+/////////////////
+
+
+
+//Functions///////
+
+function generateRandomString() {
+  let list = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let newURL = "";
+
+  for (var i = 0; i < 6; i++) {
+    newURL += list.charAt(Math.floor(Math.random() * list.length));
+  }
+  return newURL;
+}
+
+//////////////////
+
+
 
 app.listen(8080)
