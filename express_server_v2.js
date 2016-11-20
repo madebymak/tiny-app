a137793ca8e7196ae23f8b56aff7f5e8b2b03506
madebymak <mr.jmak@gmail.com>
2016-11-20 02:55:28 +0000

    Added delete route

--- express_server_v2.js
+++ express_server_v2.js
@@ -3,19 +3,21 @@ const app = express();
 const PORT = process.env.PORT || 8080
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 
 //Database/////
+
 const databaseURLs = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-const databaseUsers = {
+const users = {
   "jacky": {
     id: "jacky",
     userId: "jacky",
@@ -32,15 +34,21 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(cookieParser());
+
 app.use(cookieSession({
   name: "session",
-  keys: ["secretKey"]
+  keys: ["key1", "key2"]
 }));
 
-app.use(function (req, res, next) {
-  //TODO
-  next();
-});
+//checks for logged in user
+// app.use(function (req, res, next) {
+//   let currentUser = req.session.user;
+//   console.log("current:", currentUser)
+//   //userId == undefined ? req.body.users = undefined : req.body.users = users.users[userId];
+//   next();
+// });
+
 
 
 //Functions///////
@@ -65,16 +73,24 @@ app.get("/", function (req, res) {
 });
 
 app.get("/urls", function (req, res) {
+
   // res.send("Main page");
-  res.render("urls_index")
+  let email = req.session.email;
+  let templateVars = {email: req.body.email,
+    urls: databaseURLs};
+    console.log("temp:",templateVars);
+  res.render("urls_index", templateVars)
 });
 
 app.post("/urls", function (req, res) {
-
+  var shortURL = generateRandomString();
+  var longURL = req.body.longURL;
+  databaseURLs[shortURL] = longURL;
+  res.redirect("/urls");
 });
 
 app.get("/urls/new", function (req, res) {
-  // TODO
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", function (req, res) {
@@ -89,6 +105,11 @@ app.post("/urls/:id", function (req, res) {
   // TODO
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete databaseURLs[req.params.id];
+  res.redirect('/urls');
+});
+
 app.get("/login", function (req, res) {
   // res.send("Login");
 });
@@ -99,23 +120,28 @@ app.get("/register", function (req, res) {
 
 app.post("/register", function (req, res) {
   let userRandomId = generateRandomString();
-  let user = {id: userRandomId, email: req.body.email, password: req.body.password};
+  let user = {
+    id: userRandomId,
+    email: req.body.email,
+    password: req.body.password
+  };
 
   if (!user["email"] || !user["password"]) {
     res.status(400).send("Please enter a email and password.");
     return;
   }
 
-  for(var userId in databaseUsers) {
-    if (databaseUsers[userId].email === req.body.email) {
+  for(var userId in users) {
+    if (users[userId].email === req.body.email) {
       res.status(400).send("Email not available.");
       return;
     }
   };
 
-  databaseUsers[userRandomId] = user;
-
-  res.redirect("/urls")
+  users[userRandomId] = user;
+  console.log("user:",user);
+  console.log(users);
+  res.redirect("/")
 });
 
 app.post("/login", function (req, res) {
